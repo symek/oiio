15faa0647cccef57403185d3c2de02f8c00a05cb
symek <szymon.kapeniak@gmail.com>
2012-11-10 23:33:42 +0100

    Some work on mandatory RatInput stuff.

--- src/rat.imageio/ratinput.cpp
+++ src/rat.imageio/ratinput.cpp
@@ -39,6 +39,7 @@
 // #include "thread.h"
 // #include "strutil.h"
 // #include "fmath.h"
+#include "filesystem.h"
 
 #include "IMG/IMG_File.h"
 
@@ -51,6 +52,7 @@ public:
     RatInput () { init(); }
     virtual ~RatInput () { close(); }
     virtual const char * format_name (void) const { return "rat"; }
+    virtual bool valid_file (const std::string &filename) const;
     virtual bool open (const std::string &name, ImageSpec &newspec);
     virtual bool close ();
     virtual int current_subimage (void) const { return m_subimage; }
@@ -67,10 +69,15 @@ public:
                                     int zbegin, int zend,
                                     int firstchan, int nchans, void *data);
 private:
-    std::string m_filename;           ///< Stash the filename
-    IMG_File *m_file;                 ///< Open image handle
+    std::string   m_filename;           ///< Stash the filename
+    IMG_File      *m_file;              ///< Open image handle
+    IMG_FileParms *m_parms;
+    IMG_Stat      *m_stat;
+    IMG_Format    *m_format;
+
     int m_subimage;
     int m_miplevel;
+    int m_components;
     /// Reset everything to initial state
     void init () { m_file = NULL; }
 
@@ -84,4 +91,50 @@ OIIO_EXPORT ImageInput *rat_input_imageio_create () { return new RatInput; }
 OIIO_EXPORT int rat_imageio_version = OIIO_PLUGIN_VERSION;
 OIIO_EXPORT const char * rat_input_extensions[] = {"rat", NULL};
 OIIO_PLUGIN_EXPORTS_END
+
+
+
+
+bool RatInput::open(const std::string &name, ImageSpec &newspec) 
+{
+     // Quick check to reject non-exr files
+    if (! Filesystem::is_regular (name)) {
+        error ("Could not open file \"%s\"", name.c_str());
+        return false;
+    }
+
+     m_file = IMG_File::open(name.c_str(), m_parms, m_format);
+     // NULL if couldn't open
+     if (!m_file)
+        return false;
+
+    // File statistics:
+    m_stat = &(m_file->getStat());
+    m_components = 0;
+    // Houdini stores channels inside planes data:
+    // like C{RGB}, diff{RGB}, not R/G/B/A/diff.R/diff.G etc:
+    for (int i = 0; i < m_stat->getNumPlanes(); i++)
+    {
+        IMG_Plane *plane = m_stat->getPlane(i);
+        m_components += IMGvectorSize(plane->getColorModel()); 
+    } 
+
+    m_spec = ImageSpec(m_stat->getXres(), m_stat->getYres(), m_components, TypeDesc::UINT8);
+    #ifdef DEBUG
+            std::cerr << m_stat->getXres() << ", " << m_stat->getYres() << ", " << m_components << std::endl;
+    #endif
+    newspec = m_spec;
+    return true;
+}
+
+
+bool RatInput::close() 
+{ 
+    m_file->close(); 
+    delete m_file; 
+    init(); 
+    return true;
+}
+
+
 OIIO_PLUGIN_NAMESPACE_END
